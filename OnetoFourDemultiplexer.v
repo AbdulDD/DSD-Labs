727c28f440289df449347cc2c1cfdc3df579a72e
Abdul Muqtadir <73823824+AbdulDD@users.noreply.github.com>
2021-01-10 21:35:46 +0500

    Delete OnetoFourDemultiplexer.v

--- OnetoFourDemultiplexer.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps                // Compilation/simulation directive
-
-module OnetoFourDemultiplexer(input S1, S0, D, output Y[3:0]);
-
-// Assign statement
-assign 
-
-
-endmodule
